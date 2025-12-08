9789a6748ffd27f2f53989ed937632222e959346
duy khiem <khiemndhe181770@fpt.edu.vn>
2025-12-08 23:48:38 +0700

    add thêm outfitProvider

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,21 +1,22 @@
 import "@/styles/globals.css";
-// import Script from "next/script"; // (Nếu bạn chưa dùng thì cứ comment)
+// import Script from "next/script"; // (Chưa dùng đến thì bỏ dòng này)
 import { WardrobeProvider } from "@/context/WardrobeContext";
 import { SettingProvider } from "@/context/SettingContext";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-// 1. Import AuthProvider
 import { AuthProvider } from "@/context/AuthContext";
+import { OutfitProvider } from "@/context/OutfitContext";
 
 export default function App({ Component, pageProps }) {
   const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
 
   return (
     <GoogleOAuthProvider clientId={clientId}>
-      {/* 2. Bọc AuthProvider ở đây, bên ngoài các Provider khác */}
       <AuthProvider>
         <SettingProvider>
           <WardrobeProvider>
-            <Component {...pageProps} />
+            <OutfitProvider>
+              <Component {...pageProps} />
+            </OutfitProvider>
           </WardrobeProvider>
         </SettingProvider>
       </AuthProvider>

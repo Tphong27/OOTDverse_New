536672796c94b57e44acbcf8ed9fe42d6eb8d38b
Nguyen Thanh Phong(K18) <phongnthe182099@fpt.edu.vn>
2025-12-11 21:06:19 +0700

    update API

--- frontend/src/services/accountService.js
+++ frontend/src/services/accountService.js
@@ -1,7 +1,7 @@
 // frontend/src/services/accountService.js
 import axios from "axios";
 
-const API_URL = `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api"}/users`;
+const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/api/users`;
 
 // ============================================
 // QUẢN LÝ TÀI KHOẢN (ADMIN)

8d28b941777f1b99a21cc1358bd62aaed7395d23
duy khiem <khiemndhe181770@fpt.edu.vn>
2025-12-14 22:52:56 +0700

    update thêm provider

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
-// import Script from "next/script"; // (Chưa dùng đến thì bỏ dòng này)
 import { WardrobeProvider } from "@/context/WardrobeContext";
 import { SettingProvider } from "@/context/SettingContext";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AuthProvider } from "@/context/AuthContext";
 import { OutfitProvider } from "@/context/OutfitContext";
+import { MarketplaceProvider } from "@/context/MarketplaceContext";
+import { OrderProvider } from "@/context/OrderContext";
+import { SwapProvider } from "@/context/SwapContext";
 
 export default function App({ Component, pageProps }) {
   const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
@@ -15,7 +17,13 @@ export default function App({ Component, pageProps }) {
         <SettingProvider>
           <WardrobeProvider>
             <OutfitProvider>
-              <Component {...pageProps} />
+              <MarketplaceProvider>
+                <OrderProvider>
+                  <SwapProvider>
+                    <Component {...pageProps} />
+                  </SwapProvider>
+                </OrderProvider>
+              </MarketplaceProvider>
             </OutfitProvider>
           </WardrobeProvider>
         </SettingProvider>

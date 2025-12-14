f728e0cc5844b522658c769fec4cc758d177495f
duy khiem <khiemndhe181770@fpt.edu.vn>
2025-12-14 15:56:52 +0700

    test

--- frontend/src/components/layout/LayoutUser.jsx
+++ frontend/src/components/layout/LayoutUser.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Topbar from "./Topbar";
 import UserSidebar from "./UserSidebar";
+import Footer from "./Footer";
 
 export default function LayoutUser({ children }) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
@@ -15,9 +16,15 @@ export default function LayoutUser({ children }) {
       <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
         <Topbar setIsSidebarOpen={setIsSidebarOpen} />
 
-        <main className="flex-1 overflow-y-auto p-4 md:p-8 scroll-smooth">
-          <div className="max-w-7xl mx-auto animate-fade-in-up">{children}</div>
-        </main>
+        <div className="flex-1 overflow-y-auto scroll-smooth">
+          <main className="p-4 md:p-8">
+            <div className="max-w-7xl mx-auto animate-fade-in-up">
+              {children}
+            </div>
+          </main>
+
+          <Footer />
+        </div>
       </div>
     </div>
   );

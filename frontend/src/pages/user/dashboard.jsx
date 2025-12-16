a6154a16e09907e91f6315c3e77de722f2b6752c
duy khiem <khiemndhe181770@fpt.edu.vn>
2025-12-16 22:48:34 +0700

    up

--- frontend/src/pages/user/dashboard.jsx
+++ frontend/src/pages/user/dashboard.jsx
@@ -8,9 +8,13 @@ export default function UserDashboard() {
   return (
       <LayoutUser>
         gợi ý nội dung dashboard người dùng ở đây
+        <br></br>
         -thống kê số outfit đã tạo, số item trong tủ đồ, hoạt động gần đây, v.v.
+        <br></br>
         -liên kết nhanh đến các trang chính như tủ đồ, mix đồ AI, cộng đồng, chợ, v.v.
+        <br></br>
         -hiển thị thông tin người dùng và tùy chọn cài đặt tài khoản
+        <br></br>
         -sử dụng biểu đồ, thẻ thông tin, và các thành phần giao diện hấp dẫn để làm nổi bật dashboard
       </LayoutUser>
     );
